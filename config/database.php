084045fbfd6ce9101a2ab566b23ed03854799a24
James Kingsley <jameslkingsley@outlook.com>
2018-02-09 01:37:52 +0000

    Pull database tool

--- config/database.php
+++ config/database.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -32,7 +31,6 @@ return [
     */
 
     'connections' => [
-
         'sqlite' => [
             'driver' => 'sqlite',
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
@@ -47,6 +45,21 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
+        'mysql_live' => [
+            'driver' => 'mysql',
+            'host' => env('DB_LIVE_HOST', '127.0.0.1'),
+            'port' => env('DB_LIVE_PORT', '3306'),
+            'database' => env('DB_LIVE_DATABASE', 'forge'),
+            'username' => env('DB_LIVE_USERNAME', 'forge'),
+            'password' => env('DB_LIVE_PASSWORD', ''),
+            'unix_socket' => env('DB_LIVE_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -77,7 +90,6 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
         ],
-
     ],
 
     /*
@@ -105,7 +117,6 @@ return [
     */
 
     'redis' => [
-
         'client' => 'predis',
 
         'default' => [
@@ -114,7 +125,5 @@ return [
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
-
     ],
-
 ];
